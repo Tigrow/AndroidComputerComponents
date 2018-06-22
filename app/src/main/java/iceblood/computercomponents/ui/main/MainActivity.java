a92905c93e8fbbdfb1fa91c6501fe6277d57f4a2
Tigrow <leha_kislo@mail.ru>
2018-06-22 13:43:10 +0300

    add home image

--- app/src/main/java/iceblood/computercomponents/ui/main/MainActivity.java
+++ app/src/main/java/iceblood/computercomponents/ui/main/MainActivity.java
@@ -87,7 +87,7 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_home) {
             // Handle the camera action
         } else if (id == R.id.nav_gallery) {
 
