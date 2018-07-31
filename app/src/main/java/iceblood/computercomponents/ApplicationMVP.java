dc3bca6b773e97b6a311738ad5c98305349af0cd
Tigrow <leha_kislo@mail.ru>
2018-07-31 16:29:24 +0300

    added FireBase

--- app/src/main/java/iceblood/computercomponents/ApplicationMVP.java
+++ app/src/main/java/iceblood/computercomponents/ApplicationMVP.java
@@ -2,8 +2,11 @@ package iceblood.computercomponents;
 
 import android.app.Application;
 import android.arch.persistence.room.Room;
+import android.support.annotation.NonNull;
 import android.util.Log;
 
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.*;
 import iceblood.computercomponents.model.database.AppDataBase;
 
 /**
@@ -11,8 +14,11 @@ import iceblood.computercomponents.model.database.AppDataBase;
  */
 
 public class ApplicationMVP extends Application {
-    public static ApplicationMVP instance;
+    private static ApplicationMVP instance;
 
+
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference databaseReference;
     private AppDataBase database;
     @Override
     public void onCreate() {
@@ -21,6 +27,7 @@ public class ApplicationMVP extends Application {
         database = Room.databaseBuilder(this, AppDataBase.class, "database")
                 .build();
 
+        initFirebase();
         Log.d("lol", "onCreate: ");
     }
 
@@ -31,6 +38,26 @@ public class ApplicationMVP extends Application {
 
     public AppDataBase getDatabase() {
         return database;
+    }
+
+    private void initFirebase() {
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference("configuration/dbVersion");
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+        //FirebaseApp.initializeApp(this);
+        //получаем точку входа для базы данных
+        //mFirebaseDatabase = FirebaseDatabase.getInstance();
+        //получаем ссылку для работы с базой данных
+        //mDatabaseReference = mFirebaseDatabase.getReference();
     }
 
 }
